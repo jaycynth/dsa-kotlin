2abb8e0d86d4ed66eaaf94f8a9f5c35b684a4db6
JulieKivuva <lokoc2623@gmail.com>
2022-11-18 16:39:36 +0300

    linked list append and insert after a node

--- linkedList/LinkedList.kt
+++ linkedList/LinkedList.kt
@@ -12,7 +12,7 @@ class LinkedList<T> {
     }
 
     // Inserting at the beginning of the list
-    fun push(value: T) :LinkedList<T>{
+    fun push(value: T): LinkedList<T> {
         head = Node(value, head)
         if (tail == null) {
             tail = head
@@ -21,6 +21,43 @@ class LinkedList<T> {
 
         return this
     }
+
+    fun append(value: T): LinkedList<T> {
+        //append on an empty list if functionally identical to push
+        if (isEmpty()) {
+            push(value)
+            return this
+        }
+
+        tail?.next = Node(value = value)
+        tail = tail?.next
+        size++
+
+        return this
+    }
+
+    fun nodeAt(pos: Int): Node<T> {
+        var currentNode = head
+
+        var currentIndex = 0
+        while (currentNode != null && currentIndex < pos) {
+            currentNode = currentNode.next
+            currentIndex++
+        }
+        return currentNode!!
+    }
+
+    fun insert(value: T, afterNode: Node<T>): Node<T> {
+        if (tail == afterNode) {
+            append(value)
+            return tail!!
+        }
+        val newNode = Node(value = value, next = afterNode.next)
+        afterNode.next = newNode
+        size++
+        return newNode
+    }
+
 
     override fun toString(): String {
         return if (isEmpty()) {
@@ -36,7 +73,12 @@ fun main() {
 
     val values: LinkedList<Int> = LinkedList()
 
-    values.push(1).push(2).push(3)
+    values.append(9).push(10)
+    println(values)
 
+    values.push(1).push(2).push(3)
+    println(values)
+
+    values.append(11).append(12)
     println(values)
 }
