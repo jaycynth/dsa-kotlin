29816d419705d7cf41964e1c70482066c611e1cc
JulieKivuva <lokoc2623@gmail.com>
2022-11-21 15:25:25 +0300

    linked list pop, removeLast and remove after node

--- linkedList/LinkedList.kt
+++ linkedList/LinkedList.kt
@@ -7,7 +7,7 @@ class LinkedList<T> {
 
     private var size = 0
 
-    fun isEmpty(): Boolean {
+    private fun isEmpty(): Boolean {
         return size == 0
     }
 
@@ -58,6 +58,59 @@ class LinkedList<T> {
         return newNode
     }
 
+    fun pop(): T? {
+        if (!isEmpty()) size--
+        val result = head?.value
+        head = head?.next
+
+        if (isEmpty()) {
+            tail = null
+        }
+        //returns the popped item
+        return result
+    }
+
+    fun removeLast(): T? {
+        //if head is null return null
+        val head = head ?: return null
+
+        //if the list has one node, then remove last is functionally equal to pop
+        if (head.next == null) return pop()
+
+        size--
+
+        var prev = head
+        var current = head
+
+        var next = current.next
+
+        while (next != null) {
+            prev = current
+            current = next
+            next = current.next
+        }
+
+        prev.next = null
+        tail = prev
+        return current.value
+    }
+
+
+    fun removeAfter(node: Node<T>): T? {
+        val result = node.next?.value
+
+        if (node.next == tail) {
+            tail = node
+        }
+
+        if (node.next != null) {
+            size--
+        }
+
+        node.next = node.next?.next
+        return result
+    }
+
 
     override fun toString(): String {
         return if (isEmpty()) {
@@ -81,4 +134,22 @@ fun main() {
 
     values.append(11).append(12)
     println(values)
+
+    val middleNode = values.nodeAt(3)
+
+    values.insert(2345, middleNode)
+    println(values)
+
+    values.pop()
+    println(values)
+
+    values.removeLast()
+    println(values)
+
+    values.removeAfter(middleNode)
+    println(values)
+
+
+
+
 }
